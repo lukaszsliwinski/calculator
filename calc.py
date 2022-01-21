25e3fa5890b5a971cd375ff50cd151f66aae96ac
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2022-01-21 21:15:12 +0100

    organize code, change comments etc.

--- calc.py
+++ calc.py
@@ -10,73 +10,84 @@ Window.size = (500,700)
 
 Builder.load_file('calc.kv')
 
+
 class MyLayout(Widget):
+
     # Operation state
     operation_ended = False
-    dot_included = False
 
+
+    # Clear input and display '0'
     def clear(self):
-        self.ids.calc_input.text = "0"
+        self.ids.calc_input.text = '0'
+
 
     # Create a button pressing function
     def button_press(self, button):
-        # create a variable that contains whatever was in the textbox already
-        prior = self.ids.calc_input.text
+        # Create a variable that contains expression in the textbox already
+        exp = self.ids.calc_input.text
         # Check if operation is ended
         if self.operation_ended == True:
-            prior = ''
+            exp = ''
 
         # determine if 0 is sitting there
-        if prior == "0":
+        if exp == '0':
             self.ids.calc_input.text = ''
             self.ids.calc_input.text = f'{button}'
             # Change operation state
             self.operation_ended = False
         else:
-            self.ids.calc_input.text = f'{prior}{button}'
+            self.ids.calc_input.text = f'{exp}{button}'
+            # Change the operation state
             self.operation_ended = False
+
 
     # Create function to remove last character in textbox
     def remove(self):
-        # Remove sign only operation is not ended
+        # Remove sign only if operation is not ended
         if self.operation_ended == False:
-            prior = self.ids.calc_input.text
+            exp = self.ids.calc_input.text
             # If there is only one item in the textbox, call clear() function to display '0'
-            if len(prior) == 1:
+            if len(exp) == 1:
                 self.clear()
             else:
                 # Remove the last item in the textbox
-                prior = prior[:-1]
+                exp = exp[:-1]
                 # Output back to the textbox
-                self.ids.calc_input.text = prior
+                self.ids.calc_input.text = exp
         else:
             pass
 
-    # Create function to make textbox positive or negative
-    def pos_neg(self):
-        prior = ''
-        prior = self.ids.calc_input.text
 
-        # Test to see if there's a - sign already at the beginning
-        if prior[0] == "-":
-            prior = prior[1:]
-            self.ids.calc_input.text = f'{prior}'
+    # Create function to make number positive or negative
+    def pos_neg(self):
+        exp = ''
+        exp = self.ids.calc_input.text
+
+        # Test to see if there's a '-' sign already at the beginning
+        if exp[0] == '-':
+            exp = exp[1:]
+            self.ids.calc_input.text = f'{exp}'
         else:
-            self.ids.calc_input.text = f'-{prior}'
+            self.ids.calc_input.text = f'-{exp}'
+
 
     # Create decimal function
     def dot(self):
-        prior = self.ids.calc_input.text
+        exp = self.ids.calc_input.text
 
-        last_sign = prior[-1]
+        # Get last sign from the operation
+        last_sign = exp[-1]
 
+        # Check if last sign is an integer
         try:
             last_sign = int(last_sign)
         except ValueError:
             pass
         
+        # Get last number from the operation
         last_num = ''
-        for sign in prior[::-1]:
+        for sign in exp[::-1]:
             try:
                 sign = int(sign)
             except ValueError:
@@ -87,6 +98,7 @@ class MyLayout(Widget):
             else:
                 break
 
+        # Convert from str to correct type
         try:
             if '.' in last_num: 
                 last_num = float(last_num[::-1])
@@ -95,91 +107,111 @@ class MyLayout(Widget):
         except ValueError:
             pass
         
+        # If last number is not decimal, allow to add dot
         if type(last_num) is int:
-            prior += "."
+            exp += '.'
         else:
             pass
 
-        self.ids.calc_input.text = prior
+        self.ids.calc_input.text = exp
 
-    # create addition function
+
+    # Create math function
     def math_sign(self, sign):
-        # create a variable that contains whatever was in the textbox already
-        prior = self.ids.calc_input.text
+        exp = self.ids.calc_input.text
 
-        # if dot is in the end, remove it
-        if prior[-1] == '.':
-            prior = prior[:-1]
+        # If dot is in the end, remove it
+        if exp[-1] == '.':
+            exp = exp[:-1]
 
-        # slap a plus sign to the textbox
-        self.ids.calc_input.text = f'{prior}{sign}'
-        # Change operation state
-        self.operation_ended = False
+        # Put a math sign to the textbox
+        self.ids.calc_input.text = f'{exp}{sign}'
 
-    # create equals to function
+
+    # Create equals function
     def equals(self):
-        prior = self.ids.calc_input.text
+        exp = self.ids.calc_input.text
+
         # Error handling
         try:
             # Evaluate the math from the textbox
-            answer = eval(prior)
+            answer = eval(exp)
             # Output the answer
             self.ids.calc_input.text = str(round(answer, 6))
-            # Change operation state
+
             self.operation_ended = True
         except:
             self.display_error()
 
-
+    # Create power function
     def power(self):
-        prior = self.ids.calc_input.text
+        exp = self.ids.calc_input.text
+
+        # Evaluate power and handle an error
         try:
-            power = float(prior)**2
+            power = float(exp)**2
             self.ids.calc_input.text = str(power)
             self.operation_ended = True
         except:
             self.display_error()
 
+
+    # Create square root function
     def square_root(self):
-        prior = self.ids.calc_input.text
+        exp = self.ids.calc_input.text
+
+        # Evaluate square root and handle an error
         try:
-            square = round(math.sqrt(float(prior)), 6)
+            square = round(math.sqrt(float(exp)), 6)
             self.ids.calc_input.text = str(square)
             self.operation_ended = True
         except:
             self.display_error()
 
+
+    # Create reciprocal function
     def reciprocal(self):
-        prior = self.ids.calc_input.text
+        exp = self.ids.calc_input.text
+
+        # Evaluate reciprocal and handle an error
         try:
-            rec = round(1/float(prior), 6)
+            rec = round(1/float(exp), 6)
             self.ids.calc_input.text = str(rec)
             self.operation_ended = True
         except:
             self.display_error()
 
+
+    # Create absolute function
     def absolute(self):
+        # Evaluate operation
         self.equals()
-        prior = self.ids.calc_input.text
+        exp = self.ids.calc_input.text
 
-        if prior[0] == '-':
-            prior = prior[1:]
+        # Check if number is negative and make an absolute
+        if exp[0] == '-':
+            exp = exp[1:]
 
-        self.ids.calc_input.text = prior
+        self.ids.calc_input.text = exp
 
+
+    # Create factorial function
     def factorial(self):
-        prior = self.ids.calc_input.text
+        exp = self.ids.calc_input.text
+
+        # Convert from str to correct type and handle an error
         try:
-            prior = int(prior)
+            exp = int(exp)
         except ValueError:
             try:
-                prior = float(prior)
+                exp = float(exp)
             except ValueError:
                 self.display_error()
         
+        # Evaluate factorial and handle an error
         fact = 1
-        if (type(prior) == int or (type(prior) == float and prior.is_integer())) and prior < 16:
-            for i in range(1, int(prior + 1)):
+        if (type(exp) == int or (type(exp) == float and exp.is_integer())) and exp < 16:
+            for i in range(1, int(exp + 1)):
                 fact *= i
 
             self.ids.calc_input.text = str(fact)
@@ -188,9 +220,9 @@ class MyLayout(Widget):
         else:
             self.display_error()
 
-    
+    # Create function that displays an error
     def display_error(self):
-        self.ids.calc_input.text = "ERR"
+        self.ids.calc_input.text = 'ERR'
         self.operation_ended = True
 
 
